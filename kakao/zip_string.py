283e692e1419665ee3640c4b9e684f8d8b786f6a
ZScomnet <dark6ro@naver.com>
2021-08-28 18:17:00 +0900

    Solve 2021.08.28

--- kakao/zip_string.py
+++ kakao/zip_string.py
@@ -1,59 +1,23 @@
-# def solution(s):
-# 	answer = len(s)
-# 	for size in range(1,int(len(s)/2)+1):
-# 		for start in range(0,size):
-# 			cnt = 0
-# 			result = start
-# 			before = s[start:start+size]
-# 			for lit in range(start,len(s),size):
-# 				if before == s[lit:lit+size]:
-# 					cnt += 1
-# 				else:
-# 					if cnt == 1:
-# 						result += size
-# 					else:
-# 						result += size+1
-# 						cnt = 1
-# 					before = s[lit:lit+size]
-# 			if cnt != 1:
-# 				result += size+1
-# 			elif cnt == 1 and (len(s)-start)%size == 0:
-# 				result += size
-# 			else:
-# 				result += (len(s)-start)%size
-# 			print(start,size,result)
-# 			if result < answer:
-# 				answer = result
-
-# 	return answer
-
-def solution(s):
-	answer = len(s)
-	for size in range(1,int(len(s)/2)+1):
-		cnt = 0
-		result = ""
-		before = s[0:size]
-		for lit in range(0,len(s),size):
-			if before == s[lit:lit+size]:
-				cnt += 1
-			else:
-				if cnt == 1:
-					result += before
-				else:
-					result += str(cnt)
-					result += before
-					cnt = 1
-				before = s[lit:lit+size]
-			print(size,result)
-		
-		if (len(s)%size) != 0 and cnt == 1:
-			result += s[-(len(s)%size):]
-		elif cnt != 1:
-			result += str(cnt) + s[-size:]
+def solution(msg):
+	dic = dict()
+	for i in range(26):
+		dic[chr(ord('A')+i)] = i+1
+	answer = []
+	w = ""
+	for c in msg:
+		if w+c not in dic:
+			dic[w+c] = len(dic)+1
+			answer.append(dic[w])
+			w = c
 		else:
-			result += s[-size:]
-		if answer > len(result):
-			answer = len(result)
-	return answer
+			w += c
+	if w not in dic:
+		dic[w] = len(dic)+1
+	answer.append(dic[w])
+	print(answer)
 
-print(solution("abcabcdede"))
+
+	return
+
+if __name__ == "__main__":
+	print(solution("ABABABABABABABAB"))
