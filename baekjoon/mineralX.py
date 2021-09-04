87b7dcfbcb7b081fa155efb2f85076d6361b25b4
ZScomnet <dark6ro@naver.com>
2021-09-04 16:22:53 +0900

    Solving 2021.09.04

--- baekjoon/mineralX.py
+++ baekjoon/mineralX.py
@@ -1,47 +1,48 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
 def droptable(point,cave,R,C):
-	search = [point,]
+	search = deque([[point[0],point[1]]])
 	overlap = [[0]*C for _ in range(R)]
 	overlap[point[0]][point[1]] = 1
 	drow,dcol = [-1,0,1,0],[0,1,0,-1]
-	drop = []
-	dic = dict()
-	dic[point[1]] = point[0]
-	while len(search) != 0:
-		new_search = []
-		for p in search:
-			overlap[p[0]][p[1]] = 1
-			for row,col in zip(drow,dcol):
-				if 0 <= p[0]+row < R and 0 <= p[1]+col < C:
-					if cave[p[0]+row][p[1]+col] == 'x' and overlap[p[0]+row][p[1]+col] == 0:
-						new_search.append([p[0]+row,p[1]+col])
-						if p[0]+row == R-1:
-							return
-						if p[1]+col not in dic:
-							dic[p[1]+col] = p[0]+row
-						elif dic[p[1]+col] < p[0]+row:
-							dic[p[1]+col] = p[0]+row
-		drop += search
-		search = new_search
-	depth = R
-	for col,row in zip(dic.keys(),dic.values()):
-		for i in range(row+1,R):
-			if cave[i][col] == 'x' and i-row < depth:
-				depth = i-row-1
-				break
-			if i == R-1 and cave[i][col] == '.' and i-row < depth:
-				depth = i-row
-	for d in drop:
-		cave[d[0]][d[1]] = '.'
-	for d in drop:
-		cave[d[0]+depth][d[1]] = 'x'
+	drop = [point,]
+	if point[0] == R-1:
+		return
+	while search:
+		r,c = search.popleft()
+		overlap[r][c] = 1
+		for row,col in zip(drow,dcol):
+			if 0 <= r+row < R and 0 <= c+col < C:
+				if cave[r+row][c+col] == 'x' and overlap[r+row][c+col] == 0:
+					overlap[r+row][c+col] = 1
+					search.append([r+row,c+col])
+					drop.append([r+row,c+col])
+					if r+row == R-1:
+						return
+	drop_mineral(cave,drop,R,overlap)
 
-def print_cave(cave):
-	for row in cave:
-		for col in row:
-			print(col,end="")
+def drop_mineral(cave,drop,R,overlap):
+	fall = 101
+	for point in drop:
+		for i in range(point[0]+1,R):
+			if overlap[i][point[1]] == 1:
+				break
+			if cave[i][point[1]] == 'x':
+				if fall > i-1-point[0]:
+					fall = i-1-point[0]
+					break
+				else:
+					break
+			elif i == R-1 and fall > i-point[0]:
+				fall = i-point[0]
+	if fall != 101:
+		for point in drop:
+			cave[point[0]][point[1]] = '.'
+		for point in drop:
+			cave[point[0]+fall][point[1]] = 'x'
+	
 
 if __name__=="__main__":
 	R,C = map(int,input().split())
@@ -78,4 +79,6 @@ if __name__=="__main__":
 							droptable([R-i-1,col],cave,R,C)
 					break
 			left = True
-	print_cave(cave)
+	for row in cave:
+		print(''.join(row),end="")
+	print()
