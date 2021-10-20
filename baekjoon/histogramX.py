b8c23860d610c84fb7e0892afbc5e412aea822f8
ZScomnet <dark6ro@naver.com>
2021-10-20 16:33:33 +0900

    Solve 2021.10.20

--- baekjoon/histogramX.py
+++ baekjoon/histogramX.py
@@ -1,98 +1,65 @@
 import sys
-
 input = sys.stdin.readline
 
-def histogram(line):
-	memory = []
-	minsquare = line[0]
-	before = 1
-	answer = 0
-	mix = True
-	for i in range(len(line)):
-		if len(memory) == 0:
-			memory.append(line[i])
+def solution(N,line):
+	stack = []
+	area = min(line)*N
+	for i in line:
+		if len(stack) == 0:
+			stack.append(i)
 		else:
-			if minsquare > line[i]:
-				minsquare = line[i]
-			if memory[-1] <= line[i]:
-				memory.append(line[i])
+			if stack[-1] <= i:
+				stack.append(i)
 			else:
-				cnt = 0
-				area = 0
-				now,after = 0,0
-
-				print(memory,answer)
-				while memory:
-					now = memory.pop()
-					cnt += 1
-					if area < now*cnt:
-						area = now*cnt
-						after = now
-				if now == after and answer != 0 and mix:
-					if after == before:
-						answer += area
-						memory.append(line[i])
-						continue
-					elif after > before:
-						if answer < answer+int(area*(before/after)):
-							answer = answer+int(area*(before/after))
-							mix = True
-						elif answer < area:
-							answer = area
-							mix = True
-						else:
-							mix = False
-					else:
-						if answer < area+int(answer*(after/before)):
-							answer = area+int(answer*(after/before))
-							mix = True
-						elif answer < area:
-							answer = area
-							mix = True
-						else:
-							mix = False
+				length = 1
+				while stack:
+					if stack[-1] <= i:
+						break
+					area = max(length*stack.pop(),area)
+					length += 1
+				if stack:
+					for i in range(length):
+						stack.append(stack[-1])
 				else:
-					if answer <= area:
-						answer = area
-				before = after
-				memory.append(line[i])
-	if memory:
-		cnt = 0
-		now = 0
-		area = 0
-		while memory:
-			now = memory.pop()
-			cnt += 1
-			if area < now*cnt:
-				area = now*cnt
-				after = now
-		if answer == 0:
-			answer = area
-		elif now == after and mix:
-			if after == before:
-				answer += area
-			elif after > before:
-				if answer < answer+int(area*(before/after)):
-					answer = answer+int(area*(before/after))
-				elif answer < area:
-					answer = area
-			else:
-				if answer < area+int(answer*(after/before)):
-					answer = area+int(answer*(after/before))
-				elif answer < area:
-					answer = area
+					stack.append(i)
+
+
+	if stack:
+		length = 1
+		while stack:
+			area = max(length*stack.pop(),area)
+			length += 1
+
+	for i in line[::-1]:
+		if len(stack) == 0:
+			stack.append(i)
 		else:
-			if answer < area:
-				answer = area
-	if minsquare*len(line) > answer:
-		answer = minsquare*len(line)
-	return answer
+			if stack[-1] <= i:
+				stack.append(i)
+			else:
+				length = 1
+				while stack:
+					if stack[-1] <= i:
+						break
+					area = max(length*stack.pop(),area)
+					length += 1
+				if stack:
+					for i in range(length):
+						stack.append(stack[-1])
+				else:
+					stack.append(i)
+
+	if stack:
+		length = 1
+		while stack:
+			area = max(length*stack.pop(),area)
+			length += 1
+
+	return area
 
 if __name__ == "__main__":
 	while True:
 		line = list(map(int,input().split()))
-		if line[0] != 0:
-			print(max(histogram(line[1:]),histogram(line[1:][::-1])))
-		else:
+		if line[0] == 0:
 			break
-		
+		print(solution(line[0],line[1:]))
