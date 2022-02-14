4bd54754a282152027a55c1fe8467a9b7b504680
ZScomnet <dark6ro@naver.com>
2022-02-14 15:56:15 +0900

    Solve 2022.02.14

--- kakao/blacklist.py
+++ kakao/blacklist.py
@@ -1,28 +1,29 @@
-def collect(list_db,user_list,line):
-	if len(line) == len(user_list):
-		line_check = set(line)
-		if line_check not in list_db and len(line_check) == len(line):
-			list_db.append(set(line.copy()))
-	else:
-		for i in user_list[len(line)]:
-			line.append(i)
-			collect(list_db,user_list,line)
-			line.pop()
+from itertools import permutations
 
 def solution(user_id,banned_id):
-	user_list = [[] for _ in range(len(banned_id))]
-	for user in user_id:
-		for ban,index in zip(banned_id,range(len(banned_id))):
-			same_cnt = 0
+	answer = []
+	combi = list(permutations(user_id,len(banned_id)))
+	for i in combi:
+		is_user = True
+		for user,ban in zip(i,banned_id):
 			if len(user) == len(ban):
 				for u,b in zip(user,ban):
-					if u == b or b == '*':
-						same_cnt += 1
+					if u != b and b != '*':
+						is_user = False
+						break
+			else:
+				is_user = False
+				break
+			if not is_user:
+				break
 
-			if same_cnt == len(ban):
-				user_list[index].append(user)
-	list_db = []
-	collect(list_db,user_list,[])
-	return len(list_db)
+		if is_user:
+			temp = list(i)
+			temp.sort()
+			temp = ''.join(temp)
+			if temp not in answer:
+				answer.append(temp)
+	return len(answer)
 
-print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"],["fr*d*", "*rodo", "******", "******"]))
+if __name__ == "__main__":
+	print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"],	["fr*d*", "abc1**"]))
